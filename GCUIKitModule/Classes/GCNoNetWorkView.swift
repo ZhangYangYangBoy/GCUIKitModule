7c2d487d1ec749fbb87ae3874e37ab37daa98613
yangyangzhang <yangyangzhang17@creditease.cn>
2018-03-19 12:59:11 +0800

    修改xib 图片

--- GCUIKitModule/Classes/GCNoNetWorkView.swift
+++ GCUIKitModule/Classes/GCNoNetWorkView.swift
@@ -10,18 +10,29 @@ import UIKit
 
 public class GCNoNetWorkView: UIView {
 
-    public override init(frame: CGRect) {
-        super.init(frame: frame)
-        let imageView = UIImageView.init(frame: CGRect.init(x: 0, y: 0, width: 100, height: 100))
-        imageView.center = self.center
-        imageView.image = UIImage.init(named: "NetWorkFailed", in: Bundle(for: type(of: self)), compatibleWith: nil)
-        imageView.backgroundColor = UIColor.gray
-        self.addSubview(imageView)
-        self.backgroundColor = UIColor.yellow
+    
+    public override func awakeFromNib() {
+        super.awakeFromNib()
+        self.backgroundColor = UIColor.red
     }
     
-    public required init?(coder aDecoder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
+    public class func newInstance() -> GCNoNetWorkView {
+        let nibView = Bundle(for: GCNoNetWorkView.self).loadNibNamed("GCNoNetWorkView", owner: nil, options: nil)
+        return nibView!.first as! GCNoNetWorkView
     }
+    
+//    public override init(frame: CGRect) {
+//        super.init(frame: frame)
+//        let imageView = UIImageView.init(frame: CGRect.init(x: 0, y: 0, width: 100, height: 100))
+//        imageView.center = self.center
+//        imageView.image = UIImage.init(named: "NetWorkFailed", in: Bundle(for: type(of: self)), compatibleWith: nil)
+//        imageView.backgroundColor = UIColor.gray
+//        self.addSubview(imageView)
+//        self.backgroundColor = UIColor.yellow
+//    }
+//
+//    public required init?(coder aDecoder: NSCoder) {
+//        fatalError("init(coder:) has not been implemented")
+//    }
 
 }
